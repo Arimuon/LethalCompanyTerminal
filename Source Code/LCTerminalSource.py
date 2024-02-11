d50373b5053d6f42d3e49d311e419517560ebd3b
Ari <144230403+Arimuon@users.noreply.github.com>
2024-02-11 11:41:40 +0000

    Added error sound + added main menu

--- Source Code/LCTerminalSource.py
+++ Source Code/LCTerminalSource.py
@@ -6,6 +6,7 @@ from datetime import datetime # Used to grab the current date and time for the t
 from termcolor import colored as colour # Used to make the terminal green like Lethal Company's terminal
 import pygame as sound # Used to play sounds from the terminal
 from time import sleep # Used to make the terminal wait before executing the next line of code
+import os # Used to clear the terminal
 current_day = datetime.now().strftime('%A') # Grabs the current day in your local timezone
 credits = 60 # The default amount of credits the player has at the start of a Lethal Company game
 
@@ -13,6 +14,9 @@ def sound_play(sound_file_name):
     sound.mixer.init()
     sound.mixer.music.load(sound_file_name)
     sound.mixer.music.play()
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def startMenu():
     sound_play("TerminalStart.mp3")
@@ -29,17 +33,57 @@ def startMenu():
     print("")
     userInput = input("").lower()
     if userInput == "help":
+        clear_terminal()
         mainMenu()
     elif userInput == "exit":
         exit()
     else:
+        clear_terminal()
         print(colour("[There was no action supplied with the word.]", "green"))
-        sound("error.mp3")
+        sound_play("TerminalError.mp3")
         sleep(1)
         startMenu()
         
               
 def mainMenu():
+    print(colour(credits, "green"))
+    print("")
+    print(">MOONS \n To see the list of moons the autopilot can route to.")
+    print("")
+    print(">STORE \n To see the company store's selection of useful items.")
+    print("")
+    print(">BESTIARY \n To see the list of wildlife on record.")
+    print("")
+    print(">OTHER \n To see the list of other commands.")
+    userInput = input("").lower()
+    if userInput == "moons":
+        clear_terminal()
+        moonsMenu()
+    elif userInput == "store":
+        clear_terminal()
+        storeMenu()
+    elif userInput == "bestiary":
+        clear_terminal()
+        bestiaryMenu()
+    elif userInput == "other":
+        clear_terminal()
+        otherMenu()
+    elif userInput == "exit":
+        exit()
+    else:
+        clear_terminal()
+        print(colour("[There was no action supplied with the word.]", "green"))
+        sound_play("TerminalError.mp3")
+        sleep(1)
+        mainMenu()
+
+def moonsMenu():
+    pass
+def storeMenu():
+    pass
+def bestiaryMenu():
+    pass
+def otherMenu():
     pass
 
 # Main program
